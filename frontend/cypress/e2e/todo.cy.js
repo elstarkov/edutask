c47965c507351396726128020f6412f72d3f8810
emklemkl <emil94_karlsson@outlook.com>
2024-04-24 21:27:53 +0200

    getting started

--- frontend/cypress/e2e/todo.cy.js
+++ frontend/cypress/e2e/todo.cy.js
@@ -1,9 +1,7 @@
 
 describe('Adding task to authenticated user', () => {
     // define variables that we need on multiple occasions
-    let uid // user id
-    let name // name of the user (firstName + ' ' + lastName)
-    let email // email of the user
+    let uid, name, email // name of the user (firstName + ' ' + lastName)
 
     before(function () {
         // create a fabricated user from a fixture
@@ -16,43 +14,45 @@ describe('Adding task to authenticated user', () => {
                 body: user
             }).then((response) => {
                 uid = response.body._id.$oid
+                console.log(uid,"adssdadas,");
                 name = user.firstName + ' ' + user.lastName
                 email = user.email
-                cy.wait(500)
-                cy.request({
-                    method: 'POST',
-                    url: 'http://localhost:5000/tasks/create',
-                    form: true,
-                    body:[
-                            {
-                                'title': "hej", 
-                                'description': "a desc",
-                                'userid': response.body._id,
-                                'url': "https://www.youtube.com/watch?v=eaIvk1cSyG8&ab_channel=ThumbsUpForRockAndRoll",
-                                'todos': ['Eat a snack']
-                            }
-                        ]
-                }).then((response) => {
-                    console.log(response.body);
-                })
+
+            cy.request({
+                method: 'POST',
+                url: 'http://localhost:5000/tasks/create',
+                form: true,
+                body:
+                        {
+                            'title': "hej", 
+                            'description': "a desc",
+                            'userid': uid,
+                            'url': "https://www.youtube.com/watch?v=eaIvk1cSyG8&ab_channel=ThumbsUpForRockAndRoll",
+                            'todos': 'Eat a snack'
+                        }
+                    
+            }).then((response) => {
+                console.log(response.body);
+            })
             })
         })
     })
+    
 
     beforeEach(function () {
-        // enter the main main page
         cy.visit('http://localhost:3000')
+        cy.get('#email').type(email);
+        cy.get("form").eq(0).submit() // Send first form that is found
     })
 
     it('starting out on the landing screen', () => {
         
         // make sure the landing page contains a header with "login"
-        cy.get('#email')
-            .should('contain.text', '')
     })
 
     after(function () {
         // clean up by deleting the user from the database
+        console.log("asdasdD",uid);
         cy.request({
             method: 'DELETE',
             url: `http://localhost:5000/users/${uid}`
