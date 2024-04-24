9d83d1f98c5d64391f0843f84c1187ff4c47335d
emklemkl <emil94_karlsson@outlook.com>
2024-04-24 23:56:19 +0200

    did first test case

--- frontend/cypress/e2e/todo.cy.js
+++ frontend/cypress/e2e/todo.cy.js
@@ -1,12 +1,18 @@
 
-describe('Adding task to authenticated user', () => {
+describe('Adding task to authenticated user',  
+    {
+        env: {
+        taskDesc: "Stuff to do!",
+        },
+    },
+        () => {
     // define variables that we need on multiple occasions
-    let uid, name, email // name of the user (firstName + ' ' + lastName)
+    let uid, name, email, taskId // name of the user (firstName + ' ' + lastName)
 
     before(function () {
         // create a fabricated user from a fixture
         cy.fixture('user.json')
-            .then((user) => {
+        .then((user) => {
             cy.request({
                 method: 'POST',
                 url: 'http://localhost:5000/users/create',
@@ -14,26 +20,20 @@ describe('Adding task to authenticated user', () => {
                 body: user
             }).then((response) => {
                 uid = response.body._id.$oid
-                console.log(uid,"adssdadas,");
                 name = user.firstName + ' ' + user.lastName
                 email = user.email
-
-            cy.request({
-                method: 'POST',
-                url: 'http://localhost:5000/tasks/create',
-                form: true,
-                body:
-                        {
-                            'title': "hej", 
-                            'description': "a desc",
-                            'userid': uid,
-                            'url': "https://www.youtube.com/watch?v=eaIvk1cSyG8&ab_channel=ThumbsUpForRockAndRoll",
-                            'todos': 'Eat a snack'
-                        }
-                    
-            }).then((response) => {
-                console.log(response.body);
-            })
+                
+            cy.fixture('task.json').then((task) => {
+                task.userid = uid
+                cy.request({
+                    method: 'POST',
+                    url: 'http://localhost:5000/tasks/create',
+                    form: true,
+                    body: task
+                    }).then((response) => {
+                        taskId = response.body[0]._id.$oid
+                    })
+                })
             })
         })
     })
@@ -41,23 +41,36 @@ describe('Adding task to authenticated user', () => {
 
     beforeEach(function () {
         cy.visit('http://localhost:3000')
-        cy.get('#email').type(email);
+        cy.get('#email').should("not.be.disabled").type(email);
         cy.get("form").eq(0).submit() // Send first form that is found
+        cy.get(".container-element > a").eq(0).click()
+
+        // Create aliases.
+        cy.get(".inline-form").as("taskForm")
     })
 
-    it('starting out on the landing screen', () => {
-        
-        // make sure the landing page contains a header with "login"
+    it('Add task', () => {
+
+        cy.get("@taskForm").children("[type=text]").type(Cypress.env("taskDesc")).then(() => {
+            cy.get("@taskForm").children("[type=text]").should("have.value", Cypress.env("taskDesc"))
+            cy.get("@taskForm").children("[type=submit]").should("be.visible").click()
+        })
+        cy.get(".todo-item").children("span.editable").contains(Cypress.env("taskDesc"))
     })
 
     after(function () {
         // clean up by deleting the user from the database
-        console.log("asdasdD",uid);
         cy.request({
             method: 'DELETE',
             url: `http://localhost:5000/users/${uid}`
         }).then((response) => {
             cy.log(response.body)
         })
+        // cy.request({
+        //     method: 'DELETE',
+        //     url: `http://localhost:5000/tasks/byid/${taskId}`
+        // }).then((response) => {
+        //     cy.log(response.body)
+        // })
     })
 })
